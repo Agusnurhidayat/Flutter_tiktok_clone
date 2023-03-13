0d30e85bfc660942eeb094f8a11291557e1b457d
agus nurhidayat <agusnurhidayat900@gmail.com>
2023-03-14 03:39:36 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_mobile_tiktok_clone/pages/root_app.dart';
+
+void main(List<String> args) {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: RootApp(),
+  ));
+}
